a16b305eae91a6dad09f8bb6b420758f99e09bd4
Manfred Lange <manfredmlange@gmail.com>
2022-07-17 04:59:14 +0000

    update copyright notice

--- RimuTec.AspNetCore.SpaServices.Extensions/WebpackDevelopmentServer/WebpackDevelopmentServerMiddlewareExtensions.cs
+++ RimuTec.AspNetCore.SpaServices.Extensions/WebpackDevelopmentServer/WebpackDevelopmentServerMiddlewareExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2020 (c) RimuTec Ltd. All rights reserved.
+﻿// Copyright 2020-2022 (c) RimuTec Ltd. All rights reserved.
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 //
